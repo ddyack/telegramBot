b3066d5945c4f1dfb74a719e1688495c113f2340
Дмитрий <ddyack@MacBook-Pro-Dmitrij.local>
2020-11-09 01:05:37 +0300

    fix linters remarks

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import requests
 from yobit import get_btc
 from translate import get_translate
 
-#https://api.telegram.org/bot484689969:AAEjgfcR7__nCrxfAyOpAqQqscvI9DL-5mM/sendmessage?chat_id=475425006&text=hi
+# https://api.telegram.org/bot484689969:AAEjgfcR7__nCrxfAyOpAqQqscvI9DL-5mM/sendmessage?chat_id=475425006&text=hi
 token = '484689969:AAEjgfcR7__nCrxfAyOpAqQqscvI9DL-5mM'
 URL = 'https://api.telegram.org/bot' + token + '/'
 
@@ -30,8 +30,8 @@ def get_message():
         chat_id = last_object['message']['chat']['id']
         message_text = last_object['message']['text']
 
-        message = {'chat_id' : chat_id,
-                   'text' : message_text }
+        message = {'chat_id': chat_id,
+                   'text': message_text}
         return message
     return None
 
@@ -54,15 +54,18 @@ def run():
 
                 if text == '/btc':
                     send_message(chat_id, get_btc())
-                elif text == '/help':  # проверка на команду /help Выдаем список всех команд для бота
+                # проверка на команду /help Выдаем список всех команд для бота
+                elif text == '/help':
                     send_message(chat_id, '/btc - Узнать курс Биткоина на бирже yobit.net\n'
                                           '/translate - Через данную команду можно перевести любой вводимы текст с английского на русский')
                 elif '/translate ' in text:
-                    text_translate = text.replace('/translate ', '') # Вырезаем слово /translate
+                    # Вырезаем слово /translate
+                    text_translate = text.replace('/translate ', '')
                     text_translate = get_translate(text_translate)
 
                     send_message(chat_id, text_translate)  # пишем перевод
-                else:  # при получение любого иного текста выскакивает эта фраза
+                else:
+                    # при получение любого иного текста выскакивает эта фраза
                     send_message(chat_id, 'Я тебя не понимаю;)')
         else:
             continue
